50c40a656b8c3330b8ddfa3b30bfa69af631d774
NANDHITHA B <161127910+Nandhitha05@users.noreply.github.com>
2025-05-09 23:59:36 +0530

    Create sumk.java

---
+++ PLACEMENT_PREPARATION/ZOHO-2025/Nandhitha05/Day_01/sumk.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int presum = 0, cnt = 0;
+        Map<Integer, Integer> mpp = new HashMap<>();
+        mpp.put(0, 1);  
+
+        for (int i = 0; i < n; i++) {
+            presum += nums[i];
+            int rem = presum - k;
+            cnt += mpp.getOrDefault(rem, 0);
+            mpp.put(presum, mpp.getOrDefault(presum, 0) + 1);
+        }
+
+        return cnt;
+    }
+}
